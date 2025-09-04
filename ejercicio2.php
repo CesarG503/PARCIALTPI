ddf1af90e3d474bae037b9b555665e140cd80060
CesarG503 <cesargaray06@gmail.com>
2025-09-04 12:48:45 -0600

    feat: creacion del formulario de temperatura

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,3 +1,31 @@
 <?php
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="" method="post">
+
+    <label for=""><input type="radio" name="tipo" value="F"> fahrenheit</label>
+    
+    <Label><input type="radio" name="tipo" value="C">Celcius</Label>
+
+    <br>
+
+    <label for="temperatura">Temperatura: </label>
+    <input type="number" step="0.01" required placeholder="ingresa la temperatura" name="temperatura">
+
+    <br>
+    <input type="submit" value="CONVERTIR">
+</form>
+
+<?php ?>
+    
+</body>
+</html>
